78e8f7bab8847be9bc856ddac75e78f14f1d68c4
Andrew Reitz <areitz@nerdery.com>
2016-11-23 16:16:43 -0600

    Initial Release (1.0)

---
+++ code-style/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Installs The Nerdery's Android Studio configs into your user configs.
+
+echo "Installing Nerdery Android Studio configs..."
+
+CONFIGS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/configs"
+# escape spaces in path
+CONFIGS=$(echo $CONFIGS | sed 's/ /\\ /g')
+
+for i in $HOME/Library/Preferences/IntelliJIdea*  \
+         $HOME/Library/Preferences/IdeaIC*        \
+         $HOME/Library/Preferences/AndroidStudio* \
+         $HOME/.IntelliJIdea*/config              \
+         $HOME/.IdeaIC*/config                    \
+         $HOME/.AndroidStudio*/config
+do
+  if [[ -d $i ]]; then
+
+    # Install codestyles
+    mkdir -p $i/codestyles
+    cp -frv "$CONFIGS/codestyles"/* $i/codestyles
+
+    # Install inspections
+    mkdir -p $i/inspection
+    cp -frv "$CONFIGS/inspection"/* $i/inspection
+
+    # Install options ("Exclude from Import and Completion")
+    mkdir -p $i/options
+    cp -frv "$CONFIGS/options"/* $i/options
+
+    # Install file templates
+    mkdir -p $i/fileTemplates/includes
+    cp -fv "$CONFIGS/fileTemplates/includes/File Header.java" $i/fileTemplates/includes
+  fi
+done
+
+echo "Done."
+echo ""
+echo "Restart Android Studio, go to preferences, and apply 'NerderyAndroid'."
