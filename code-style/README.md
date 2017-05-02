8f5dc0e4894bf3a4340d7b3f2caf0f52494036dc
Richard Banasiak <richard@banasiak.com>
2017-05-02 12:49:46 -0500

    Added code-style directory and moved standards.adoc into the standards directory.

---
+++ code-style/README.md
@@ -0,0 +1,23 @@
+Java Code Styles
+================
+
+IntelliJ IDEA code style settings for The Nerdery's Android projects.
+
+
+Installation
+------------
+
+ * On Unix, run the `install.sh` script. Windows users should use `install.bat` instead.
+ * Restart IntelliJ if it's running.
+ * Open IntelliJ Project Settings -> Code Styles, change the code style for the
+   project to the one you want.
+
+Credit
+------
+
+Based on the Android code style customized by [Square](https://github.com/square/java-code-styles)
+
+License
+-------
+
+[![Public domain](https://licensebuttons.net/p/zero/1.0/88x31.png)](https://creativecommons.org/publicdomain/zero/1.0/legalcode)
---
+++ code-style/install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Installs The Nerdery's IntelliJ configs into your user configs.
+
+echo "Installing Nerdery IntelliJ configs..."
+
+CONFIGS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/configs"
+
+for i in $HOME/Library/Preferences/IntelliJIdea*  \
+         $HOME/Library/Preferences/IdeaIC*        \
+         $HOME/Library/Preferences/AndroidStudio* \
+         $HOME/.IntelliJIdea*/config              \
+         $HOME/.IdeaIC*/config                    \
+         $HOME/.AndroidStudio*/config
+do
+  if [[ -d $i ]]; then
+
+    # Install codestyles
+    mkdir -p $i/codestyles
+    cp -frv "$CONFIGS/codestyles"/* $i/codestyles
+
+    # Install inspections
+    mkdir -p $i/inspection
+    cp -frv "$CONFIGS/inspection"/* $i/inspection
+
+    # Install options ("Exclude from Import and Completion")
+    mkdir -p $i/options
+    cp -frv "$CONFIGS/options"/* $i/options
+
+    # Install colors
+    mkdir -p $i/colors
+    cp -frv "$CONFIGS/colors"/* $i/colors
+
+    # Install templates
+    mkdir -p $i/templates
+    cp -frv "$CONFIGS/templates"/* $i/templates
+
+  fi
+done
+
+echo "Done."
+echo ""
+echo "Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'NerderyAndroid'."
